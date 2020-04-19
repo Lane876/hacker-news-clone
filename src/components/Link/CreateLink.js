8692235f17950714291b15b854bc1f9abcb0aa43
Lane876 <milenkovicmilan10@gmail.com>
2020-04-19 15:03:42 +0200

    added firestore

--- src/components/Link/CreateLink.js
+++ src/components/Link/CreateLink.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import { FirebaseContext } from "../../firebase";
 import useFormValidation from "../Auth/useFormValidation";
 import validateCreateLink from "../Auth/validateCreateLink";
 
@@ -8,6 +9,7 @@ const INITIAL_STATE = {
 };
 
 function CreateLink(props) {
+  const { firebase, user } = useContext(FirebaseContext);
   const { handleSubmit, handleChange, values, errors } = useFormValidation(
     INITIAL_STATE,
     validateCreateLink,
@@ -15,7 +17,25 @@ function CreateLink(props) {
   );
 
   function handleCreateLink() {
-    console.log("link has been created");
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      const { url, description } = values;
+      const newLink = {
+        url,
+        description,
+        postedBy: {
+          id: user.uid,
+          name: user.displayName,
+        },
+        votes: [],
+        comments: [],
+        created: Date.now(),
+      };
+
+      firebase.db.collection("links").add(newLink);
+      props.history.push("/");
+    }
   }
 
   return (
@@ -34,7 +54,7 @@ function CreateLink(props) {
         name="url"
         placeholder="The URL for the link"
         autoComplete="off"
-        type="text"
+        type="url"
         onChange={handleChange}
         value={values.url}
         className={errors.url && "error-input"}
