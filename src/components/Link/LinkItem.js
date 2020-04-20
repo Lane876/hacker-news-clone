a47fb3c1de3c3a82c1307ab887ffb820573ec678
Lane876 <milenkovicmilan10@gmail.com>
2020-04-20 08:40:58 +0200

    CRUD

--- src/components/Link/LinkItem.js
+++ src/components/Link/LinkItem.js
@@ -1,13 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, withRouter } from "react-router-dom";
 import { getDomain } from "../../utils";
-import distanceInWordsToNow from "date-fns/formatDistanceToNow";
-function LinkItem({ link, index, showCount }) {
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import { FirebaseContext } from "../../firebase";
+
+function LinkItem({ link, index, showCount, history }) {
+  const { firebase, user } = useContext(FirebaseContext);
+
+  function handleVote() {
+    if (!user) {
+      history.push("/login");
+    } else {
+      //must have link reference in order to get it and this is it
+      const voteRef = firebase.db.collection("links").doc(link.id);
+      //must use get() method in order to get the spec link /like referencig it like above is not good enough
+      voteRef.get().then((doc) => {
+        if (doc.exists) {
+          const previousVotes = doc.data().votes;
+          const vote = { voteBy: { id: user.uid, name: user.displayName } };
+          //updating votes
+          const updatedVotes = [...previousVotes, vote];
+          voteRef.update({ votes: updatedVotes });
+        }
+      });
+    }
+  }
+
+  const postedByAuthUser = user && user.uid === link.postedBy.id;
+
+  function handleDeleteButton() {
+    const linkRef = firebase.db.collection("links").doc(link.id);
+    linkRef
+      .delete()
+      .then(() => {
+        console.log(`Document with this ID ${link.id} deleted`);
+      })
+      .catch((err) => {
+        console.error("Error deleting document:", err);
+      });
+  }
+
   return (
     <div className="flex items-start mt2">
       <div className="flex items-center">
         {showCount && <span className="gray">{index}.</span>}
-        <div className="vote-button">▲</div>
+        <div className="vote-button" onClick={handleVote}>
+          ▲
+        </div>
       </div>
       <div className="ml1">
         <div>
@@ -16,17 +55,25 @@ function LinkItem({ link, index, showCount }) {
         </div>
         <div className="f6 lh-copy gray">
           {link.votes.length} votes by {link.postedBy.name}{" "}
-          {distanceInWordsToNow(link.created)}
+          {formatDistanceToNow(link.created)}
           {" | "}
           <Link to={`/link/${link.id}`}>
             {link.comments.length > 0
               ? `${link.comments.length} comments`
               : "discuss"}
           </Link>
+          {postedByAuthUser && (
+            <>
+              {" | "}
+              <span className="delete-button" onClick={handleDeleteButton}>
+                delete
+              </span>
+            </>
+          )}
         </div>
       </div>
     </div>
   );
 }
 
-export default LinkItem;
+export default withRouter(LinkItem);
